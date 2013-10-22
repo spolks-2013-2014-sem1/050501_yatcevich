625526b0b326c6c9bd16ba6e98a55e814624387b
Peter Yatcevich <tehnolog.by@gmail.com>
2013-10-22 15:32:31 +0300

    Nothing. Removed some ' '. Added some ' '.

--- lab2/main.c
+++ lab2/main.c
@@ -23,7 +23,8 @@ int main(int argc, char *argv[])
 	memset(buf, 0, BUF_SIZE);			 
 	memset(&server, 0, sizeof(server));
 	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);	// opening socket, domain = AF_INET, type = SOCK_STREAM, protocol = 0 (default, TCP)
+	// opening socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);	// domain = AF_INET, type = SOCK_STREAM, protocol = 0 (default, TCP)
 	
 	if (sockfd < 0) {
 		perror("Opening socket error");
@@ -36,18 +37,18 @@ int main(int argc, char *argv[])
 	scanf("%d", &port);
 	server.sin_port = htons(port);
 	
-	if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0){
+	if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		perror("Binding error");
 		exit(0);
 	}
 	
 	printf("server is working\n");
 	
-	while(1){
+	while(1) {
 		listen(sockfd, 1);	// request queue size = 1
 		client = accept(sockfd, NULL, NULL);
 				
-		if (client < 0){ 
+		if (client < 0) { 
 			perror("Accept error");
 			exit(0);
 		}
